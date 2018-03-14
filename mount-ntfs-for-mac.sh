6340706ad93e352e98bad6d08de90fe093feb2df
evan <evan7fan@outlook.com>
2018-03-14 12:17:08 +0800

    auto script

--- mount-ntfs-for-mac.sh
+++ mount-ntfs-for-mac.sh
@@ -1,25 +1,9 @@
 #!/bin/sh
-diskutil list
-echo "please select disk(eg. 1s2): \c"
-read disk
-disk="disk$disk"
-sel_disk=`diskutil list | grep $disk`
-if [ -n "$sel_disk" ]
-then
-    echo "Select $sel_disk"
-else
-    echo "Don't found $disk"
-    exit 1
-fi
-
-if [ -d ~/$disk ]
-then
-    sudo umount /dev/$disk
-    rm -rf ~/$disk
-else
-    mkdir ~/$disk
-    sudo umount /dev/$disk
-    sudo mount_ntfs -o rw,nobrowse /dev/$disk ~/$disk
-    open ~/$disk
-fi
+sudo -s
+mv /sbin/mount_ntfs /sbin/mount_ntfs_orig
+echo '#!/bin/bash' > /sbin/mount_ntfs
+echo 'mount_ntfs_orig -o rw,auto,nobrowse $7 "$8"' >> /sbin/mount_ntfs
+chmod a+x /sbin/mount_ntfs
+ln -s /Volumes ~/磁盘
+exit 0
 
---
+++ unmount-ntfs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo mv /sbin/mount_ntfs_orig /sbin/mount_ntfs
